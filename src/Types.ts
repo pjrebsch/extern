be4b6145b0a39dce688a994a8190becea6a44d88
Patrick Rebsch <pjrebsch@users.noreply.github.com>
2026-04-18 21:33:15 -0400

    chore: 🗑️ remove unused disambiguation property

--- src/Types.ts
+++ src/Types.ts
@@ -9,7 +9,6 @@ export type $$Mode = "typed" | "validated";
 
 export interface $$Disambiguation {
   readonly named?: string;
-  readonly at?: number;
 }
 
 export type $$Params<$In> = $$Disambiguation & { given?: $In };
